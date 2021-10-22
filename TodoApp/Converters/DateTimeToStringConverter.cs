113ac73461009aa7d77555a2c2c7f5b9671123ef
Tobias Hoefer <Tobias.Hoefer@krones.com>
2021-10-22 07:50:42 +0200

    Moved Resources to their own ResourceDictionarys. Added DateTimeToStringConverter.

---
+++ TodoApp/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TodoApp.Converters
+{
+    class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is DateTime dt)
+            {
+                return dt.ToString("dddd, HH:mm:ss", new CultureInfo("de-DE"));
+            } else
+            {
+                return Binding.DoNothing;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
